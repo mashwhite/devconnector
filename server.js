87f460fd7006f351a818eb1eb8e4ec3359db81fc
Mashariki White <mashariki.white@gmail.com>
2018-07-10 15:11:42 -0700

    added routes

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
+
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+
 const app = express();
 
 // DB Config
@@ -11,9 +16,13 @@ mongoose
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("What's Up Riki!!"));
-// Listen on port 5000 (cause why not) But i disagree
-//this commit is nice but really not needed. 
+app.get("/", (req, res) => res.send(`she said What's Up Riki5000!!`));
+
+// Use Routes
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
+
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log("Server running on port ${port}"));
+app.listen(port, () => console.log(`Server running on port ${port}`));
