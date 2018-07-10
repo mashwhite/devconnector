e7e12e951c26fe8a8e4b5d391e9375f4c6f47be9
Mashariki White <masharikiwhite@Masharikis-MacBook-Pro.local>
2018-07-09 23:31:59 -0700

    connect MongoDB

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://mashwhite:Mlab1!@ds231961.mlab.com:31961/devconnector"
+};
--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hey World Riki!!"));
+// DB Config
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => res.send("What's Up Riki!!"));
 
 const port = process.env.PORT || 5000;
 
