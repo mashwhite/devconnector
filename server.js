6d85e3cc5cd93143d70d3e4ad29718a5c50e0f68
Mashariki White <mashariki.white@gmail.com>
2018-07-10 09:59:19 -0700

    commented server file

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ mongoose
   .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("What's Up Riki!!"));
-
+// Listen on port 5000 (cause why not)
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log("Server running on port ${port}"));
