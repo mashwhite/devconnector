36157f949b859e08616aa1ffbb274d21b11a056a
Mashariki White <mashariki.white@gmail.com>
2018-07-17 11:41:39 -0700

    Add react bootstrp navbar

--- models/Post.js
+++ models/Post.js
@@ -14,9 +14,6 @@ const PostSchema = new Schema({
   name: {
     type: String
   },
-  avatar: {
-    type: String
-  },
   likes: [
     {
       user: {
@@ -36,9 +33,6 @@ const PostSchema = new Schema({
         required: true
       },
       name: {
-        type: String
-      },
-      avatar: {
         type: String
       },
       date: {
